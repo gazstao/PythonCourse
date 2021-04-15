771f71f9772dfff11efb82bec71aa9a512be314a
gazstao <gazstao@gmail.com>
2021-04-15 09:18:34 -0300

    finded bug after insertion

--- gzt-lab-06.py
+++ gzt-lab-06.py
@@ -6,11 +6,12 @@ import json
 import pymongo
 import sys
 
-dbName = "gztlab"
+dbName = "gzt06"
 collName = "data20210414"
 arquivo = "/Users/gazstao/github/covid-19-data/public/data/latest/owid-covid-latest.json"
+dados = ""
 
-print("\n\n\n\n\n\n\nGazstao DataParserExperiment v0.05 2021-04-15 8h59")
+print("\n\n\n\n\n\n\nGazstao DataParserExperiment v0.06 2021-04-15 9h09")
 
 
 #
@@ -19,10 +20,11 @@ print("\n\n\n\n\n\n\nGazstao DataParserExperiment v0.05 2021-04-15 8h59")
 
 def file2data(arquivo):
 
-    print("\nArquivo {} carregado com sucesso!".format(arquivo))
-
     with open(arquivo) as file:
+
         dados = file.read()
+        print("\nArquivo {} carregado com sucesso!".format(arquivo))
+
         objJSON = json.loads(dados)
         return objJSON
 
@@ -31,25 +33,32 @@ def file2data(arquivo):
 #     Lista dados
 #
 
-def listaDados(listaJSON):
-    listaDados = []
-    for item in listaJSON:
-        listaDados.append(listaJSON[item])
-        print("{}){}".format(item, listaJSON[item]["location"]), end="\t\t\t\t")
-        if (len(listaDados)%3 == 0):
-            print("")
-    print ("A lista tem {} itens.".format(len(listaDados)))
+def listaDados(lista):
+
+    qty = 0
+    for item in lista:
+
+        qty += 1
+        print("{}){}".format(item, lista[item]["location"]), end="\t\t\t\t")
+
+        if (qty%3 == 0):
+            print("") # pula linha
+
+    print ("A lista tem {} itens.".format(qty)
 
 
 #
 #       Função insere dados
 #
 
-def insereDados(listaJSON, dbConn):
+def insereDados(dados, dbConn):
+
+    listaJSON = json.loads(dados)
     for item in listaJSON:
-        print(json.dumps(item),end=" ")
-        # print(json.dumps(objJSON[item], indent = 4, sort_keys = True))
+
         objID = dbConn.insert_one(listaJSON[item]).inserted_id
+
+        print(json.dumps(item),end=" ")
         print("Objeto {} inserido com sucesso.".format(objID))
 
 #
@@ -89,31 +98,44 @@ dadosJSON = file2data(arquivo)
 
 resposta = input ("\nDeseja carregar os dados para o Banco de Dados?\t")
 if (resposta == "y" or resposta == "Y"):
+
     print("Carregando dados para {}".format(prodCollection))
     insereDados(dadosJSON, prodCollection)
+
 else:
+
     print("Passando...")
 
 resposta = input ("\nDeseja visualizar os dados?\t")
 if (resposta == "y" or resposta == "Y"):
+
     repete = True
     listaDados(dadosJSON)
+
 else:
+
     print("Passando...")
     repete = False
 
 while repete:
+
     item = input ("\nDeseja ver quais informações? ")
+
     if (item == "exit"):
         repete = False
+
     else:
+
         if (item == "" or item == "?"):
             listaDados(dadosJSON)
             print("Digite exit para sair")
+
         else:
+
             try:
                 print(json.dumps(dadosJSON[item], indent = 4, sort_keys = True))
+
             except:
-                print("Não foi possível carregar os dados.")
+                print("Não foi possível carregar os dados. Erro: {}".format(sys.exc_info()[0]))
 
 print("")
