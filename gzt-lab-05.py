df77aadbde1fcdcfe48019c357bcfcfcfdc7dc82
gazstao <gazstao@gmail.com>
2021-04-14 22:38:34 -0300

    Working fine Updated

--- gzt-lab-05.py
+++ gzt-lab-05.py
@@ -1,22 +1,25 @@
 # gazstao 2021-04-12 10h
-# Objetivo: carregar os dados JSON obtidos do arquivo para o MongoDB
+# Carrega dados JSON obtidos do arquivo para o MongoDB e mostra
+# https://github.com/owid/covid-19-data/tree/master/public/data
 
 import json
 import pymongo
 import sys
 
-dbName = "gzt-lab"
+dbName = "gztlab"
 collName = "data20210414"
 arquivo = "/Users/gazstao/github/covid-19-data/public/data/latest/owid-covid-latest.json"
 
-print("\n\nGazstao DataParserExperiment 2021-04-13 19h16\n")
+print("\n\n\n\n\n\n\nGazstao DataParserExperiment 2021-04-13 19h16")
+
+
 #
 #       Leitura do arquivo e criação de objeto JSON
 #
 
 def file2data(arquivo):
 
-    print("Arquivo :"+arquivo)
+    print("\nArquivo {} carregado com sucesso!".format(arquivo))
 
     with open(arquivo) as file:
         dados = file.read()
@@ -24,15 +27,30 @@ def file2data(arquivo):
         objJSON = json.loads(dados)
         return objJSON
 
+
 #
-#       Função Lista Dados
+#     Lista dados
 #
 
-def insereDados(objJSON, dbConn):
-    for item in objJSON:
+def listaDados(lista):
+    listaDados = []
+    for item in lista:
+        listaDados.append(lista[item])
+        print("{}){}".format(item, lista[item]["location"]), end="\t\t\t\t")
+        if (len(listaDados)%3 == 0):
+            print("")
+    print ("A lista tem {} itens.".format(len(listaDados)))
+
+
+#
+#       Função insere dados
+#
+
+def insereDados(listaJSON, dbConn):
+    for item in listaJSON:
         print(json.dumps(item),end=" ")
         # print(json.dumps(objJSON[item], indent = 4, sort_keys = True))
-        objID = dbConn.insert_one(objJSON[item]).inserted_id
+        objID = dbConn.insert_one(listaJSON[item]).inserted_id
         print("Objeto {} inserido com sucesso.".format(objID))
 
 #
@@ -41,11 +59,9 @@ def insereDados(objJSON, dbConn):
 
 def testaMongo(myclient):
     for eachDB in myclient.list_database_names():
-        print("\n Banco de Dados:\t {}".format(eachDB))
-
         mydb = myclient[eachDB]
         for item in mydb.list_collection_names():
-            print("    Collection:\t\t {}".format(item))
+            print("\t{}:{}".format(eachDB,item))
 
 #
 #       Mongo Conection
@@ -53,10 +69,9 @@ def testaMongo(myclient):
 
 try:
 
-    # conecta no mongoDB
     conStr = "mongodb://localhost:27017/"
     myclient = pymongo.MongoClient(conStr)
-    print("Cliente criado :{}".format(conStr))
+    print("\nConectado :{}".format(conStr))
     testaMongo(myclient)
 
 except:
@@ -72,6 +87,34 @@ prodDB = myclient[dbName]
 prodCollection = prodDB[collName]
 
 dadosJSON = file2data(arquivo)
-insereDados(dadosJSON, prodCollection)
+
+resposta = input ("\nDeseja carregar os dados para o Banco de Dados?\t")
+if (resposta == "y" or resposta == "Y"):
+    print("Carregando dados para {}".format(prodCollection))
+    insereDados(dadosJSON, prodCollection)
+else:
+    print("Passando...")
+
+resposta = input ("\nDeseja visualizar os dados?\t")
+if (resposta == "y" or resposta == "Y"):
+    repete = True
+    listaDados(dadosJSON)
+else:
+    print("Passando...")
+    repete = False
+
+while repete:
+    item = input ("\nDeseja ver quais informações? ")
+    if (item == "exit"):
+        repete = False
+    else:
+        if (item == "" or item == "?"):
+            listaDados(dadosJSON)
+            print("Digite exit para sair")
+        else:
+            try:
+                print(json.dumps(dadosJSON[item], indent = 4, sort_keys = True))
+            except:
+                print("Não foi possível carregar os dados.")
 
 print("")
